8a481ec3367792b773f6a987ab1b8b3fc5967047
zjl <289590076@qq.com>
2019-04-01 09:25:35 +0800

    添加get/set

--- src/main/java/com/zhuang/domian/User.java
+++ src/main/java/com/zhuang/domian/User.java
@@ -5,14 +5,42 @@ import lombok.ToString;
 
 import java.io.Serializable;
 
-@Data
-@ToString
 public class User implements Serializable {
 
     private Long id;
     private String name;
     private String address;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
